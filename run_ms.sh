8ed21c3af2a141acfa2da100247dd3890a05f27a
Shaun C Read <shaun.c.read@gmail.com>
2019-05-03 14:06:23 +0100

    conda activate->source

--- run_ms.sh
+++ run_ms.sh
@@ -2,11 +2,9 @@
 set -e
 source ~/.bashrc
 ENVIRONMENT="gaia-xd"
-conda activate "$ENVIRONMENT" || ( conda env create -f environment.yml && conda activate $ENVIRONMENT && pip install -r requirements.txt)
+source activate "$ENVIRONMENT" || conda env create -f environment.yml
+source activate "$ENVIRONMENT"
+pip install -r requirements.txt
 export MKL_THREADING_LAYER="GNU"
-
-# go to directory of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR"
 
 python run_ms.py async_20190310174529.vot 20 1e-5 5000 "$DIR"
