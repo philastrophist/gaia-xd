5e33c34ed0e177c12f602cef23d43301b0cab3a6
Shaun C Read <shaun.c.read@gmail.com>
2019-05-07 16:31:01 +0100

    more components

--- run_ms.sh
+++ run_ms.sh
@@ -7,4 +7,4 @@ source activate "$ENVIRONMENT"
 pip install -r requirements.txt
 export MKL_THREADING_LAYER="GNU"
 
-python run_ms.py async_20190310174529.vot 20 1e-5 5000 .
+python run_ms.py async_20190310174529.vot 40 1e-5 10000 .
